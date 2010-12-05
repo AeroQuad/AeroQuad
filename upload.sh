fb79155d34518450affd6f00b0d51212326d6ba6
Øyvind Løkling <oyvind@lokling.com>
2010-12-05 21:40:55 +0100

    buildtools: Updated upload.sh with command from arduino

--- upload.sh
+++ upload.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-avrdude -F -p atmega1280 -c stk500 -P /dev/tty.usbserial-A700exmC -b 57600 -U flash:w:AeroQuad.cpp.hex 
+#avrdude -F -p atmega1280 -c stk500 -P /dev/tty.usbserial-A700exmC -b 57600 -U flash:w:AeroQuad.cpp.hex
+export ARDUINO_AVRDUDE_CONF=/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf
+avrdude -C$ARDUINO_AVRDUDE_CONF  -patmega1280 -cstk500v1 -P/dev/tty.usbserial-A700exmC -b57600 -D -Uflash:w:AeroQuad.cpp.hex:i
