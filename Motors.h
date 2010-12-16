b87f032f333e34e4d4d83f59de86a8924e698265
CupOfTea <CupOfTea@paradise.net.nz>
2010-12-16 14:52:20 +1300

    Should compile on 328 now...  be surprised if the 8 bit timer working optimally (if at all)

--- Motors.h
+++ Motors.h
@@ -205,7 +205,7 @@ private:
 // EXPERIMENTAL uses system timers directly instead of analogWrite
 /*Some basics about the 16 bit timer:
 - The timer counts clock ticks derived from the CPU clock. Using 16MHz CPU clock
-  and a prescaler of 8 gives a timer clock of 2MHz, one tick every 0.5�s. This
+  and a prescaler of 8 gives a timer clock of 2MHz, one tick every 0.5us. This
   is also called timer resolution.
 - The timer is used as cyclic upwards counter, the counter periode is set in the
   ICRx register. IIRC periode-1 has to be set in the ICRx register.
@@ -217,7 +217,7 @@ In the code below, the periode shall be 3.3ms (300hz), so the ICRx register is
 The high time shall be 1000us, so the OCRxy register is set to 2000. In the code
  below this can be seen in the line "commandAllMotors(1000);"  A change of
  the timer periode does not change this setting, as the clock rate is still one
- tick every 0.5�s. If the prescaler was changed, the OCRxy register value would
+ tick every 0.5us. If the prescaler was changed, the OCRxy register value would
  be different. 
 */
 class Motors_PWM2 : public Motors {
@@ -235,46 +235,46 @@ private:
   void initialize(void) {
 #if defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) 
     DDRE = DDRE | B00111000;                                  // Set ports to output PE3-5 
-#if defined(plusConfig) || defined(XConfig) 
+  #if defined(plusConfig) || defined(XConfig) 
     DDRH = DDRH | B00001000;                                  // Set port to output PH3 
-#endif
-#if defined(HEXACOAXIAL) || defined(HEXARADIAL)
+  #endif
+  #if defined(HEXACOAXIAL) || defined(HEXARADIAL)
     DDRH = DDRH | B00111000;                                  // Set ports to output PH3-5  
+  #endif
 #endif
-#endif	/* __AVR_ATmega1280__) */
 #if defined (__AVR_ATmega328P__)
     DDRB = DDRB | B00001110;                                  // Set ports to output PB1-3 
     DDRD = DDRD | B00010000;                                  // Set port to output PD4 
-#endif	/* (__AVR_ATmega328P__)*/
+#endif
     commandAllMotors(1000);                                   // Initialise motors to 1000us (stopped)
 #if defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) 
     // Init PWM Timer 3                                       // WGMn1 WGMn2 WGMn3  = Mode 14 Fast PWM, TOP = ICRn ,Update of OCRnx at BOTTOM 
-    TCCR3A =((1<<WGM31)|(1<<COM3A1)|(1<<COM3B1)|(1<<COM3C1)); // Clear OCRnA/OCRnB/OCRnC outputs on compare match, set OCRnA/OCRnB/OCRnC outputs at BOTTOM (non-inverting mode)
+    TCCR3A = (1<<WGM31)|(1<<COM3A1)|(1<<COM3B1)|(1<<COM3C1);  // Clear OCnA/OCnB/OCnC on compare match, set OCnA/OCnB/OCnC at BOTTOM (non-inverting mode)
     TCCR3B = (1<<WGM33)|(1<<WGM32)|(1<<CS31);                 // Prescaler set to 8, that gives us a resolution of 0.5us 
-    ICR3 = TOP;                                              // Clock_speed / ( Prescaler * desired_PWM_Frequency) #defined above.
-#if defined(plusConfig) || defined(XConfig) 
+    ICR3 = TOP;                                               // Clock_speed / ( Prescaler * desired_PWM_Frequency) #defined above.
+  #if defined(plusConfig) || defined(XConfig) 
     // Init PWM Timer 4
-    TCCR4A =((1<<WGM41)|(1<<COM4A1)); 
+    TCCR4A = (1<<WGM41)|(1<<COM4A1); 
     TCCR4B = (1<<WGM43)|(1<<WGM42)|(1<<CS41);
     ICR4 = TOP;  
-#endif /* (plusConfig) || defined(XConfig) */
-#if defined(HEXACOAXIAL) || defined(HEXARADIAL)
+  #endif
+  #if defined(HEXACOAXIAL) || defined(HEXARADIAL)
     // Init PWM Timer 4
-    TCCR4A =((1<<WGM41)|(1<<COM4A1)|(1<<COM4B1)|(1<<COM4C1)); 
+    TCCR4A = (1<<WGM41)|(1<<COM4A1)|(1<<COM4B1)|(1<<COM4C1); 
     TCCR4B = (1<<WGM43)|(1<<WGM42)|(1<<CS41);
     ICR4 = TOP;
-#endif   /* (HEXACOAXIAL) || defined(HEXARADIAL) */
-#endif	/* __AVR_ATmega1280__) */
+  #endif
+#endif
 #if defined (__AVR_ATmega328P__)
     // Init PWM Timer 1  16 bit
-    TCCR1A =((1<<WGM11)|(1<<COM1A1)|(1<<COM1B1));
+    TCCR1A = (1<<WGM11)|(1<<COM1A1)|(1<<COM1B1);
     TCCR1B = (1<<WGM13)|(1<<WGM12)|(1<<CS11); 
     ICR1 == TOP;
     // Init PWM Timer 2   8bit
-    TCCR2A =((1<<WGM21)|(1<<COM2A1)|(1<<COM2B1)); 
-    TCCR2B = (1<<WGM23)|(1<<WGM22)|(1<<CS21);
-    ICR2 = TOP;
-#endif	/* (__AVR_ATmega328P__)*/w
+    TCCR2A = (1<<WGM21)|(1<<COM2A1)|(1<<COM2B1); 
+    TCCR2B = (1<<WGM22)|(1<<CS21)|(1<<WGM21);
+    // TOP is fixed at 255
+#endif
 }
   void write(void) {
 #if defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) 
@@ -282,17 +282,17 @@ private:
     OCR3C = motorCommand[REAR] * 2 ; 
     OCR3A = motorCommand[RIGHT] * 2 ;
     OCR4A = motorCommand[LEFT] * 2 ; 
-#if defined(HEXACOAXIAL) || defined(HEXARADIAL)
+  #if defined(HEXACOAXIAL) || defined(HEXARADIAL)
     OCR4B = motorCommand[RIGHT2] * 2 ;
     OCR4C = motorCommand[LEFT2] * 2 ; 
-#endif   /* (HEXACOAXIAL) || defined(HEXARADIAL) */
-#endif	/* (__AVR_ATmega1280__) */
+  #endif
+#endif
 #if defined (__AVR_ATmega328P__)
     OCR2B = motorCommand[FRONT] * 2 ;
     OCR1A = motorCommand[REAR] * 2 ;
-    OCR1B = motorCommand[RIGHT] * 2 ;
+    OCR1B = motorCommand[RIGHT] * 2 ;  // div???
     OCR2A = motorCommand[LEFT] * 2 ;
-#endif	/* (__AVR_ATmega328P__) */
+#endif
   }
  void commandAllMotors(int _motorCommand) {   // Sends commands to all motors
 #if defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) 
@@ -300,17 +300,17 @@ private:
     OCR3C = _motorCommand * 2 ;
     OCR3A = _motorCommand * 2 ;
     OCR4A = _motorCommand * 2 ;
-#if defined(HEXACOAXIAL) || defined(HEXARADIAL)
+  #if defined(HEXACOAXIAL) || defined(HEXARADIAL)
     OCR4B = _motorCommand * 2 ;
     OCR4C = _motorCommand * 2 ;
-#endif   /* (HEXACOAXIAL) || defined(HEXARADIAL) */
-#endif	/* (__AVR_ATmega1280__) */
+  #endif
+#endif
 #if defined (__AVR_ATmega328P__)
     OCR2B = _motorCommand * 2 ;
     OCR1A = _motorCommand * 2 ;
     OCR1B = _motorCommand * 2 ;
     OCR2A = _motorCommand * 2 ;
-#endif	/* (__AVR_ATmega328P__) */
+#endif
   }
 };
 
