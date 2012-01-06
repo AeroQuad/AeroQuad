b2324a1a071e6988cd322eeacbaf19294188a735
Kenny9999 <paticemainville@hotmail.com>
2012-01-06 20:33:52 +0100

    update of mag and fix gyto low pass filter

--- Libraries/AQ_Compass/Magnetometer_HMC5883L.h
+++ Libraries/AQ_Compass/Magnetometer_HMC5883L.h
@@ -27,62 +27,33 @@
 #include "Arduino.h"
 
 #define COMPASS_ADDRESS 0x1E
+#define COMPASS_IDENTITY 0x10
 
-// See HMC58x3 datasheet for more information on these values
-#define NormalOperation             0x10
-// Default DataOutputRate is 10hz on HMC5843 , 15hz on HMC5883L
-#define DataOutputRate_Default      ( 0x04 << 2 )
-#define HMC5883L_SampleAveraging_8  ( 0x03 << 5 )
-
-
-float magCalibration[3] = {0.0,0.0,0.0};
+#define COMPASS_ADDRESS 0x1E
+#define COMPASS_IDENTITY 0x10
+//#define SENSOR_GAIN 0x00  // +/- 0.7 Ga
+#define SENSOR_GAIN 0x20  // +/- 1.0 Ga (default)
+//#define SENSOR_GAIN 0x40  // +/- 1.5 Ga
+//#define SENSOR_GAIN 0x60  // +/- 2.0 Ga
+//#define SENSOR_GAIN 0x80  // +/- 3.2 Ga
+//#define SENSOR_GAIN 0xA0  // +/- 3.8 Ga
+//#define SENSOR_GAIN 0xC0  // +/- 4.5 Ga
+//#define SENSOR_GAIN 0xE0  // +/- 6.5 Ga (not recommended)
 
 void initializeMagnetometer() {
 
-  byte numAttempts = 0;
-  bool success = false;
   delay(10);                             // Power up delay **
 
-  magCalibration[XAXIS] = 1.0;
-  magCalibration[YAXIS] = 1.0;
-  magCalibration[ZAXIS] = 1.0;
-
-  if (readWhoI2C(COMPASS_ADDRESS) != COMPASS_ADDRESS) {
+  if (readWhoI2C(COMPASS_ADDRESS) == COMPASS_IDENTITY) {
 	  vehicleState |= MAG_DETECTED;
   }
 
-  while (success == false && numAttempts < 5 ) {
-
-    numAttempts++;
-
-    updateRegisterI2C(COMPASS_ADDRESS, 0x00, 0x11);  // Set positive bias configuration for sensor calibraiton
-    delay(50);
-    updateRegisterI2C(COMPASS_ADDRESS, 0x01, 0x20); // Set +/- 1G gain
-    delay(10);
-    updateRegisterI2C(COMPASS_ADDRESS, 0x02, 0x01);  // Perform single conversion
-    delay(10);
-
-    measureMagnetometer(0.0, 0.0);                    // Read calibration data
-    delay(10);
-
-    if ( fabs(measuredMagX) > 500.0 && fabs(measuredMagX) < 1564.4f
-        && fabs(measuredMagY) > 500.0 && fabs(measuredMagY) < 1564.4f
-        && fabs(measuredMagZ) > 500.0 && fabs(measuredMagZ) < 1477.2f) {
-      magCalibration[XAXIS] = fabs(1264.4f / measuredMagX);
-      magCalibration[YAXIS] = fabs(1264.4f / measuredMagY);
-      magCalibration[ZAXIS] = fabs(1177.2f / measuredMagZ);
-      success = true;
-    }
-
-    updateRegisterI2C(COMPASS_ADDRESS, 0x00, HMC5883L_SampleAveraging_8 | DataOutputRate_Default | NormalOperation);
-    delay(50);
-
-    updateRegisterI2C(COMPASS_ADDRESS, 0x02, 0x00); // Continuous Update mode
-    delay(50);                           // Mode change delay (1/Update Rate) **
-  }
-
+  updateRegisterI2C(COMPASS_ADDRESS, 0x01, SENSOR_GAIN); // Gain as defined above
+  delay(20);
+  updateRegisterI2C(COMPASS_ADDRESS, 0x02, 0x01); // start single conversion
+  delay(20);
+  
   measureMagnetometer(0.0, 0.0);  // Assume 1st measurement at 0 degrees roll and 0 degrees pitch
-
 }
 
 void measureMagnetometer(float roll, float pitch) {
@@ -107,25 +78,26 @@ void measureMagnetometer(float roll, float pitch) {
   #else
     //!! Define 5883L Orientation !!
   #endif
+  
+  updateRegisterI2C(COMPASS_ADDRESS, 0x02, 0x01); // start single conversion
 
   measuredMagX = rawMag[XAXIS] + magBias[XAXIS];
   measuredMagY = rawMag[YAXIS] + magBias[YAXIS];
   measuredMagZ = rawMag[ZAXIS] + magBias[ZAXIS];
 
+  const float cosRoll =  cos(roll);
+  const float sinRoll =  sin(roll);
+  const float cosPitch = cos(pitch);
+  const float sinPitch = sin(pitch);
 
-  float cosRoll =  cos(roll);
-  float sinRoll =  sin(roll);
-  float cosPitch = cos(pitch);
-  float sinPitch = sin(pitch);
+  const float magX = (float)measuredMagX * cosPitch +
+                     (float)measuredMagY * sinRoll * sinPitch +
+                     (float)measuredMagZ * cosRoll * sinPitch;
 
-  float magX = (float)measuredMagX * cosPitch +
-         (float)measuredMagY * sinRoll * sinPitch +
-         (float)measuredMagZ * cosRoll * sinPitch;
+  const float magY = (float)measuredMagY * cosRoll -
+                     (float)measuredMagZ * sinRoll;
 
-  float magY = (float)measuredMagY * cosRoll -
-         (float)measuredMagZ * sinRoll;
-
-  float tmp = sqrt(magX * magX + magY * magY);
+  const float tmp = sqrt(magX * magX + magY * magY);
 
   hdgX = magX / tmp;
   hdgY = -magY / tmp;
