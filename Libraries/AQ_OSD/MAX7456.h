20960d8a67887def4b3f6ec1a7f98cac39f9f45c
Kari Hautio <khautio@gmail.com>
2012-01-09 15:37:25 +0200

    MAX7456: fix display on negative current

--- Libraries/AQ_OSD/MAX7456.h
+++ Libraries/AQ_OSD/MAX7456.h
@@ -341,7 +341,14 @@ void displayVoltage(byte areMotorsArmed) {
     else {
       currentValue = batteryData[osdBatNo].current*10.0;
     }
-    snprintf(buf,12,"%3d.%1dA%4u\24", currentValue/10,currentValue%10,(unsigned)batteryData[osdBatNo].usedCapacity);
+    
+    if (abs(currentValue)>=100) { // > 10A only display whole amps
+      snprintf(buf,12,"%4dA%5d\24  ", currentValue/10, (int)batteryData[osdBatNo].usedCapacity);
+    }
+    else {
+      snprintf(buf,12,"%c%1d.%1dA%5d\24  ", currentValue<0?'-':' ',abs(currentValue/10),abs(currentValue%10),(int)batteryData[osdBatNo].usedCapacity);
+    }      
+      
     writeChars( buf, 11, 0, VOLTAGE_ROW+osdBatNo, VOLTAGE_COL+6 );
   }
 
