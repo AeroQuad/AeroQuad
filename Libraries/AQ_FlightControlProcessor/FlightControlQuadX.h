57db4464aeccb9506d732118059725d93fda8e80
Kenny9999 <paticemainville@hotmail.com>
2012-02-01 21:09:32 +0100

    remove throttle correction, seem to add some instability, mostly on quad

--- Libraries/AQ_FlightControlProcessor/FlightControlQuadX.h
+++ Libraries/AQ_FlightControlProcessor/FlightControlQuadX.h
@@ -49,6 +49,7 @@ int motorMinCommand[4] = {0,0,0,0};
 int motorConfiguratorCommand[4] = {0,0,0,0};
 
 void applyMotorCommand() {
+  // Front = Front/Right, Back = Left/Rear, Left = Front/Left, Right = Right/Rear 
   motorCommand[FRONT_LEFT]  = throttle - motorAxisCommandPitch + motorAxisCommandRoll - (YAW_DIRECTION * motorAxisCommandYaw);
   motorCommand[FRONT_RIGHT] = throttle - motorAxisCommandPitch - motorAxisCommandRoll + (YAW_DIRECTION * motorAxisCommandYaw);
   motorCommand[REAR_LEFT]   = throttle + motorAxisCommandPitch + motorAxisCommandRoll + (YAW_DIRECTION * motorAxisCommandYaw);
