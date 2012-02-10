289abed00137fe0d58a6da80113f6e4bd285aaa9
Ted Carancho <ted@carancho.com>
2012-02-10 01:42:56 -0800

    increased precision of transmitter cal to 6 decimal places

--- AeroQuad/SerialCom.h
+++ AeroQuad/SerialCom.h
@@ -118,14 +118,16 @@ void readSerialCommand() {
       //for(byte channel = XAXIS; channel<LASTCHANNEL; channel++) {
       //  receiverSlope[channel] = readFloatSerial();
       //}
-      receiverSlope[readFloatSerial()] = readFloatSerial();
+      channelCal = (int)readFloatSerial();
+      receiverSlope[channelCal] = readFloatSerial();
       break;
       
     case 'H': // Receive transmitter calibration values
       //for(byte channel = XAXIS; channel<LASTCHANNEL; channel++) {
       //  receiverOffset[channel] = readFloatSerial();
       //}
-      receiverOffset[readFloatSerial()] = readFloatSerial();
+      channelCal = (int)readFloatSerial();
+      receiverOffset[channelCal] = readFloatSerial();
       break;
       
     case 'I': // Initialize EEPROM with default values
@@ -370,7 +372,8 @@ void sendSerialTelemetry() {
     
   case 'g': // Send transmitter calibration data
     for (byte axis = XAXIS; axis < LASTCHANNEL; axis++) {
-      PrintValueComma(receiverSlope[axis]);
+      Serial.print(receiverSlope[axis], 6);
+      Serial.print(',');
     }
     SERIAL_PRINTLN();
     queryType = 'X';
@@ -378,7 +381,8 @@ void sendSerialTelemetry() {
     
   case 'h': // Send transmitter calibration data
     for (byte axis = XAXIS; axis < LASTCHANNEL; axis++) {
-      PrintValueComma(receiverOffset[axis]);
+      Serial.print(receiverOffset[axis], 6);
+      Serial.print(',');
     }
     SERIAL_PRINTLN();
     queryType = 'X';
