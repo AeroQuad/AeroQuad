40fa04ca5cb977bbab9eba643f03e37c8bf9200c
Charles Wood <imchipwood@CrackBookBro.local>
2013-01-19 11:44:04 -0800

    Revert "Fix to mode switching"
    This reverts commit 3e27ff64473a844e26598286de3619b9b4842db7.
    
    Reverted again to get rid of ds_store (OS X file in every folder)

--- AeroQuad/FlightCommandProcessor.h
+++ AeroQuad/FlightCommandProcessor.h
@@ -254,18 +254,13 @@ void readPilotCommands() {
     }
   }
 
-    // Check Mode switch for Acro or Stable
-    if (receiverCommand[MODE] > 1500) {
-        flightMode = ATTITUDE_FLIGHT_MODE;
-        lastFlightMode = ATTITUDE_FLIGHT_MODE;
-    }
-    else {
-        flightMode = RATE_FLIGHT_MODE;
-        if (lastFlightMode != RATE_FLIGHT_MODE) { // reset integral error when switching from attitude to rate mode
-            zeroIntegralError();
-            lastFlightMode = RATE_FLIGHT_MODE;
-        }
-    }
+  // Check Mode switch for Acro or Stable
+  if (receiverCommand[MODE] > 1500) {
+    flightMode = ATTITUDE_FLIGHT_MODE;
+  }
+  else {
+    flightMode = RATE_FLIGHT_MODE;
+  }
 
 
   #if defined AltitudeHoldBaro || defined AltitudeHoldRangeFinder
