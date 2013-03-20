91e61427e5fc33e73f80c4df883ed0570439df1d
norem <tdavis@synergistics.org>
2013-03-20 02:59:44 -0300

    Update MAX7456_VO.h

--- Libraries/AQ_OSD/MAX7456_VO.h
+++ Libraries/AQ_OSD/MAX7456_VO.h
@@ -46,11 +46,11 @@
 //
 //
 
-#define VO_DISPLAY_RECT_HEIGHT 9 										// height of rectangle bounding AI. Should be odd so that there is an equal space above/below the centre reticle
+#define VO_DISPLAY_RECT_HEIGHT 9 						// height of rectangle bounding AI. Should be odd so that there is an equal space above/below the centre reticle
 #define VO_MAX_PITCH_ANGLE (PI/8)											// bounds of scale used for displaying variometer. When variometer is >= |this number|, the variometer lines will be at top or bottom of bounding box
-#define VO_RETICLE_ROW (MAX_screen_rows/2)									// center row - don't change this
+#define VO_RETICLE_ROW (MAX_screen_rows/2)					// center row - don't change this
 #define VO_RETICLE_COL 27													// reticle will be in this column
-#define VO_CENTRE (VO_RETICLE_ROW*18+10)									// row, in pixels, corresponding to zero vario.
+#define VO_CENTRE (VO_RETICLE_ROW*18+10)					// row, in pixels, corresponding to zero vario.
 #define VO_TOP_PIXEL ((VO_RETICLE_ROW - VO_DISPLAY_RECT_HEIGHT/2)*18)		// top of bounding box of variometer display 
 #define VO_BOTTOM_PIXEL ((VO_RETICLE_ROW + VO_DISPLAY_RECT_HEIGHT/2)*18)	// bottom of bounding box of variometer display 
 
@@ -59,9 +59,9 @@ static char VO_reticle[1] = {228};											// right pointing arrow character
 static const byte VO_COLUMN = 28;											// column where the variometer line is printed
 static byte VO_AHoldline = 0;
 
-static float safe_deltaAltitudeSeconds = 1.75;								// reasonable change in altitude in feet per second to land softly
-static float safe_attitudeAngle = 0.12;										// how level to assume we're not moving excessively
-static float scaleVariometer = 3.00;										// scale to top and bottom of center pointer
+static float safe_deltaAltitudeSeconds = 1.75;					// reasonable change in altitude in feet per second to land softly
+static float safe_attitudeAngle = 0.12;						// how level to assume we're not moving excessively
+static float scaleVariometer = 3.00;						// scale to top and bottom of center pointer
 
 /**********************************************************
  ************* Landing Assist DisplayLanding **************
@@ -75,7 +75,7 @@ void displayVariometer(float climb_fallRate) {
     writeChars( VO_reticle, 1, 1, VO_RETICLE_ROW, VO_RETICLE_COL -1);		// display center pointer flashing at artificial horizon
   }
   else {  
-    writeChars( NULL, 1, 1, VO_RETICLE_ROW, VO_RETICLE_COL -1);				// erase center pointer flashing at artificial horizon
+    writeChars( NULL, 1, 1, VO_RETICLE_ROW, VO_RETICLE_COL -1);			// erase center pointer flashing at artificial horizon
 
     VO_reticle[0] = 13;														// set center pointer up-down arrow icon
 	 
@@ -87,7 +87,7 @@ void displayVariometer(float climb_fallRate) {
       VO_reticle[0] = 15;													// set center pointer to down arrow icon
     }
 
-    writeChars( VO_reticle, 1, 0, VO_RETICLE_ROW, VO_RETICLE_COL );			// display variometer center pointer
+    writeChars( VO_reticle, 1, 0, VO_RETICLE_ROW, VO_RETICLE_COL );		// display variometer center pointer
   }
 
 /**********************************************************
@@ -104,7 +104,7 @@ void displayVariometer(float climb_fallRate) {
   }
 
   char VO_RollLine = LINE_ROW_0 + (row % 18);
-  writeChars( &VO_RollLine, 1, 0, VO_AHoldline, VO_COLUMN );				// display variometer value as a bar graph line
+  writeChars( &VO_RollLine, 1, 0, VO_AHoldline, VO_COLUMN );			// display variometer value as a bar graph line
 }
 
 #endif  // #define_AQ_OSD_MAX7456_VO_H_
