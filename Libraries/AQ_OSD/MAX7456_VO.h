2cc5d9cb714d56b2d6b36d6ef973dd2fcba4c60a
norem <tdavis@synergistics.org>
2013-03-20 03:04:48 -0300

    Update MAX7456_VO.h
    major rewrite to allow delta altitude to be used elsewhere in code

--- Libraries/AQ_OSD/MAX7456_VO.h
+++ Libraries/AQ_OSD/MAX7456_VO.h
@@ -46,17 +46,20 @@
 //
 //
 
-#define VO_DISPLAY_RECT_HEIGHT 9 						// height of rectangle bounding AI. Should be odd so that there is an equal space above/below the centre reticle
-#define VO_MAX_PITCH_ANGLE (PI/8)											// bounds of scale used for displaying variometer. When variometer is >= |this number|, the variometer lines will be at top or bottom of bounding box
+#define VO_DISPLAY_RECT_HEIGHT 9 						// height of rectangle bounding AI. Should be odd so that
+										// there is an equal space above/below the centre reticle
+#define VO_MAX_PITCH_ANGLE (PI/8)						// bounds of scale used for displaying variometer. When variometer
+										// is >= |this number|, the variometer lines will be at top or bottom
+										// of bounding box
 #define VO_RETICLE_ROW (MAX_screen_rows/2)					// center row - don't change this
-#define VO_RETICLE_COL 27													// reticle will be in this column
+#define VO_RETICLE_COL 27							// reticle will be in this column
 #define VO_CENTRE (VO_RETICLE_ROW*18+10)					// row, in pixels, corresponding to zero vario.
 #define VO_TOP_PIXEL ((VO_RETICLE_ROW - VO_DISPLAY_RECT_HEIGHT/2)*18)		// top of bounding box of variometer display 
 #define VO_BOTTOM_PIXEL ((VO_RETICLE_ROW + VO_DISPLAY_RECT_HEIGHT/2)*18)	// bottom of bounding box of variometer display 
 
-static char VO_reticle[1] = {228};											// right pointing arrow character
+static char VO_reticle[1] = {228};						// right pointing arrow character
 
-static const byte VO_COLUMN = 28;											// column where the variometer line is printed
+static const byte VO_COLUMN = 28;						// column where the variometer line is printed
 static byte VO_AHoldline = 0;
 
 static float safe_deltaAltitudeSeconds = 1.75;					// reasonable change in altitude in feet per second to land softly
@@ -71,20 +74,20 @@ void displayVariometer(float climb_fallRate) {
   if (((kinematicsAngle[XAXIS] > safe_attitudeAngle)||(kinematicsAngle[XAXIS] < -safe_attitudeAngle))||
       ((kinematicsAngle[YAXIS] > safe_attitudeAngle)||(kinematicsAngle[YAXIS] < -safe_attitudeAngle)))
   { 
-    VO_reticle[0] = 227;													// we're not level enough to land ie probably moving ??
+    VO_reticle[0] = 227;							// we're not level enough to land ie probably moving ??
     writeChars( VO_reticle, 1, 1, VO_RETICLE_ROW, VO_RETICLE_COL -1);		// display center pointer flashing at artificial horizon
   }
-  else {  
+  else {
     writeChars( NULL, 1, 1, VO_RETICLE_ROW, VO_RETICLE_COL -1);			// erase center pointer flashing at artificial horizon
 
-    VO_reticle[0] = 13;														// set center pointer up-down arrow icon
+    VO_reticle[0] = 13;								// set center pointer up-down arrow icon
 	 
     if (climb_fallRate > safe_deltaAltitudeSeconds) {
-      VO_reticle[0] = 14;													// set center pointer to up arrow icon
+      VO_reticle[0] = 14;							// set center pointer to up arrow icon
     }
 
     if (climb_fallRate < -safe_deltaAltitudeSeconds) {
-      VO_reticle[0] = 15;													// set center pointer to down arrow icon
+      VO_reticle[0] = 15;							// set center pointer to down arrow icon
     }
 
     writeChars( VO_reticle, 1, 0, VO_RETICLE_ROW, VO_RETICLE_COL );		// display variometer center pointer
@@ -94,11 +97,11 @@ void displayVariometer(float climb_fallRate) {
  ****************** Draw Altitude Difference **************
  **********************************************************/
 
-  byte row = constrain(										                           				// resolution .10  -4.5 to +4.5
+  byte row = constrain(								// resolution .10  -4.5 to +4.5
   AH_CENTRE + (14.5 - (float)VO_COLUMN) * 12 * 1.4 * ( climb_fallRate/scaleVariometer ) +
     (( climb_fallRate/scaleVariometer )/VO_MAX_PITCH_ANGLE*(VO_CENTRE-VO_TOP_PIXEL)),VO_TOP_PIXEL, VO_BOTTOM_PIXEL)-1;
 
-  if ((row/18) != VO_AHoldline) {											// erase existing character if not on center row
+  if ((row/18) != VO_AHoldline) {						// erase existing character if not on center row
     writeChars( NULL, 1, 0, VO_AHoldline, VO_COLUMN );
     VO_AHoldline = row/18;
   }
