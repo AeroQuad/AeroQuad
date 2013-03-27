0b9a1bae2ed62e363012cd1b22f4d7aba7e426dd
Charles Wood <imchipwood@CrackBookBro.local>
2013-03-27 03:36:05 -0700

    Minor fixes + SBUS dynamic RX
    Hope I structured this right, Kenny… Let me know if the SBUS stuff is
    totally wrong, I'm not quite sure what you're doing since AQ32 and MEGA
    are done quite differently.
    
    Altitude hold throttle adjust fix - constraining wasn't happening at
    the right times, if zDampening was enabled, the change in throttle
    could be 2x min/maxThrottleAdjust.

--- Libraries/AQ_Receiver/Receiver_AQ32.h
+++ Libraries/AQ_Receiver/Receiver_AQ32.h
@@ -1,22 +1,22 @@
 /*
-  AeroQuad v3.0.1 - February 2012
-  www.AeroQuad.com
-  Copyright (c) 2012 Ted Carancho.  All rights reserved.
-  An Open Source Arduino based multicopter.
+ AeroQuad v3.0.1 - February 2012
+ www.AeroQuad.com
+ Copyright (c) 2012 Ted Carancho.  All rights reserved.
+ An Open Source Arduino based multicopter.
  
-  This program is free software: you can redistribute it and/or modify 
-  it under the terms of the GNU General Public License as published by 
-  the Free Software Foundation, either version 3 of the License, or 
-  (at your option) any later version. 
-
-  This program is distributed in the hope that it will be useful, 
-  but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-  GNU General Public License for more details. 
-
-  You should have received a copy of the GNU General Public License 
-  along with this program. If not, see <http://www.gnu.org/licenses/>. 
-*/
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _AEROQUAD_RECEIVER_AQ32_H_
 #define _AEROQUAD_RECEIVER_AQ32_H_
@@ -32,15 +32,15 @@
 
 
 /*
-	ROLL     0	3
-	PITCH    1	1
-	YAW      2	0
-	THROTTLE 3	2
-	MODE     4	4
-	AUX      5	6
-	AUX2     6	5
-	AUX3     7	7
-*/
+ ROLL     0	3
+ PITCH    1	1
+ YAW      2	0
+ THROTTLE 3	2
+ MODE     4	4
+ AUX      5	6
+ AUX2     6	5
+ AUX3     7	7
+ */
 
 static byte receiverPin[] = {
     Port2Pin('D', 12),
@@ -51,7 +51,7 @@ static byte receiverPin[] = {
     Port2Pin('E', 11),
     Port2Pin('E', 13),
     Port2Pin('E', 14)
-  };
+};
 
 static byte ReceiverChannelMapPWM[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // default mapping
 
@@ -62,117 +62,117 @@ static byte ReceiverChannelMapPWM[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; //
 extern voidFuncPtr PWM_in_handler[];
 
 typedef struct {
-  timer_dev   *TimerDev;
-  timer_gen_reg_map *TimerRegs;
-  __io uint32	*Timer_ccr;
-  int			Low;
-  int			High;
-  uint16		HighTime;
-  uint16		RiseTime;
-  uint16		LastChange;
-  int			Channel;
-  int			TimerChannel;
-  int			PolarityMask;
-  int			Valid;
-  int			Debug;
+    timer_dev   *TimerDev;
+    timer_gen_reg_map *TimerRegs;
+    __io uint32	*Timer_ccr;
+    int			Low;
+    int			High;
+    uint16		HighTime;
+    uint16		RiseTime;
+    uint16		LastChange;
+    int			Channel;
+    int			TimerChannel;
+    int			PolarityMask;
+    int			Valid;
+    int			Debug;
 } tFrqDataPWM;
 
 #define FRQInputs 8
 volatile tFrqDataPWM FrqDataPWM[FRQInputs];
 
 void FrqInit(int aChannel, int aDefault, volatile tFrqDataPWM *f, timer_dev *aTimer, int aTimerChannel) {
-
-  aTimerChannel--;  // transform timer channel numbering from 1-4 to 0-3
-
-  f->Channel      = aChannel;
-  f->Valid        = false;
-
-  f->TimerDev     = aTimer;
-  timer_gen_reg_map *timer = aTimer->regs.gen;
-  f->TimerRegs    = timer;
-
-  f->Timer_ccr    = &timer->CCR1 + aTimerChannel;
-  f->Debug        = false;
-  f->HighTime     = aDefault;
-  f->TimerChannel = aTimerChannel;
-
-  int TimerEnable = (1 << (4*aTimerChannel));
-  f->PolarityMask = TimerEnable << 1;
-
-  uint32 clock_speed = rcc_dev_timer_clk_speed(f->TimerDev->clk_id);
-  timer->PSC	= (clock_speed/1000000)-1;
-  timer->ARR	= 0xffff;
-  timer->CR1	= 0;
-  timer->DIER &= ~(1);
-
-  timer->CCER &= ~TimerEnable; // Disable timer
-  timer->CCER &= ~(f->PolarityMask);
-
-  volatile uint32 *mr;
-  if(aTimerChannel < 2) {
-    mr = &(timer->CCMR1);
-  }
-  else {
-    mr = &(timer->CCMR2);
-  }
-  *mr &= ~(0xFF << (8*(aTimerChannel&1)));	// prescaler 1
-  *mr |= 0x61 << (8*(aTimerChannel&1));		// 0x61 -> 6=filter, 1=inputs 1,2,3,4
-
-  timer->CCER |= TimerEnable; // Enable
-  timer->CR1 = 1;
+    
+    aTimerChannel--;  // transform timer channel numbering from 1-4 to 0-3
+    
+    f->Channel      = aChannel;
+    f->Valid        = false;
+    
+    f->TimerDev     = aTimer;
+    timer_gen_reg_map *timer = aTimer->regs.gen;
+    f->TimerRegs    = timer;
+    
+    f->Timer_ccr    = &timer->CCR1 + aTimerChannel;
+    f->Debug        = false;
+    f->HighTime     = aDefault;
+    f->TimerChannel = aTimerChannel;
+    
+    int TimerEnable = (1 << (4*aTimerChannel));
+    f->PolarityMask = TimerEnable << 1;
+    
+    uint32 clock_speed = rcc_dev_timer_clk_speed(f->TimerDev->clk_id);
+    timer->PSC	= (clock_speed/1000000)-1;
+    timer->ARR	= 0xffff;
+    timer->CR1	= 0;
+    timer->DIER &= ~(1);
+    
+    timer->CCER &= ~TimerEnable; // Disable timer
+    timer->CCER &= ~(f->PolarityMask);
+    
+    volatile uint32 *mr;
+    if(aTimerChannel < 2) {
+        mr = &(timer->CCMR1);
+    }
+    else {
+        mr = &(timer->CCMR2);
+    }
+    *mr &= ~(0xFF << (8*(aTimerChannel&1)));	// prescaler 1
+    *mr |= 0x61 << (8*(aTimerChannel&1));		// 0x61 -> 6=filter, 1=inputs 1,2,3,4
+    
+    timer->CCER |= TimerEnable; // Enable
+    timer->CR1 = 1;
 }
 
 
 void InitFrqMeasurementPWM() {
-
-  for(int rcLine = 0; rcLine < (int)(sizeof(receiverPin) / sizeof(receiverPin[0])); rcLine++) {
-    int pin = receiverPin[rcLine];
-    timer_dev *timer_num = PIN_MAP[pin].timer_device;
-    if(timer_num != NULL) {
-      gpio_set_mode(PIN_MAP[pin].gpio_device, PIN_MAP[pin].gpio_bit, GPIO_AF_INPUT_PD);
-      FrqInit(rcLine, 1500, &FrqDataPWM[rcLine], timer_num, PIN_MAP[pin].timer_channel);
-      timer_attach_interrupt(timer_num, PIN_MAP[pin].timer_channel, PWM_in_handler[rcLine]);
+    
+    for(int rcLine = 0; rcLine < (int)(sizeof(receiverPin) / sizeof(receiverPin[0])); rcLine++) {
+        int pin = receiverPin[rcLine];
+        timer_dev *timer_num = PIN_MAP[pin].timer_device;
+        if(timer_num != NULL) {
+            gpio_set_mode(PIN_MAP[pin].gpio_device, PIN_MAP[pin].gpio_bit, GPIO_AF_INPUT_PD);
+            FrqInit(rcLine, 1500, &FrqDataPWM[rcLine], timer_num, PIN_MAP[pin].timer_channel);
+            timer_attach_interrupt(timer_num, PIN_MAP[pin].timer_channel, PWM_in_handler[rcLine]);
+        }
     }
-  }
 }
 
 
 void PWMInvertPolarity(volatile tFrqDataPWM *f) {
-  f->TimerRegs->CCER ^= f->PolarityMask; // invert polarity
+    f->TimerRegs->CCER ^= f->PolarityMask; // invert polarity
 }
 
 void FrqChange(volatile tFrqDataPWM *f) {
-
-  timer_gen_reg_map *timer = f->TimerRegs;
-  uint16_t c = *(f->Timer_ccr);
-  bool rising = (timer->CCER & f->PolarityMask) == 0;
-
-  if(f->Valid) {
-    if(rising) {
-      f->RiseTime = c;
-    } 
-    else {
-      uint16_t highTime = c - f->RiseTime;
-      if(highTime > 900 && highTime < 2100) {
-        f->HighTime = highTime;
-      } 
-      else {
-        f->Valid = false;
-      }
+    
+    timer_gen_reg_map *timer = f->TimerRegs;
+    uint16_t c = *(f->Timer_ccr);
+    bool rising = (timer->CCER & f->PolarityMask) == 0;
+    
+    if(f->Valid) {
+        if(rising) {
+            f->RiseTime = c;
+        }
+        else {
+            uint16_t highTime = c - f->RiseTime;
+            if(highTime > 900 && highTime < 2100) {
+                f->HighTime = highTime;
+            }
+            else {
+                f->Valid = false;
+            }
+        }
     }
-  } 
-  else if(rising) {
-    // rising edge, store start time
-    f->RiseTime = c;
-    f->Valid = true;
-  }
-
-  PWMInvertPolarity(f);
+    else if(rising) {
+        // rising edge, store start time
+        f->RiseTime = c;
+        f->Valid = true;
+    }
+    
+    PWMInvertPolarity(f);
 }
 
 // hide the class details from the interrupt handler
 void IrqChangeValue(int chan) {
-  FrqChange(&FrqDataPWM[chan]);
+    FrqChange(&FrqDataPWM[chan]);
 }
 
 
@@ -194,7 +194,7 @@ voidFuncPtr PWM_in_handler[] = { PWM_in_0, PWM_in_1, PWM_in_2, PWM_in_3, PWM_in_
 // interface part starts here
 
 void initializeReceiverPWM() {
-
+    
 	InitFrqMeasurementPWM();
 }
 
@@ -230,86 +230,86 @@ byte   currentChannel;
 // implementation part starts here.
 
 typedef struct {
-  timer_dev   *TimerDev;
-  timer_gen_reg_map *TimerRegs;
-  __io uint32	*Timer_ccr;
-  uint16		RiseTime;
-  int			TimerChannel;
-  int			PolarityMask;
+    timer_dev   *TimerDev;
+    timer_gen_reg_map *TimerRegs;
+    __io uint32	*Timer_ccr;
+    uint16		RiseTime;
+    int			TimerChannel;
+    int			PolarityMask;
 } tFrqDataPPM;
 
 volatile tFrqDataPPM FrqDataPPM;
 
 void FrqInit(int aDefault, timer_dev *aTimer, int aTimerChannel)
 {
-  aTimerChannel--;  // transform timer channel numbering from 1-4 to 0-3
-
-  FrqDataPPM.TimerDev     = aTimer;
-  timer_gen_reg_map *timer = aTimer->regs.gen;
-  FrqDataPPM.TimerRegs    = timer;
-
-  FrqDataPPM.Timer_ccr    = &timer->CCR1 + aTimerChannel;
-  FrqDataPPM.TimerChannel = aTimerChannel;
-
-  int TimerEnable = (1 << (4*aTimerChannel));
-  FrqDataPPM.PolarityMask = TimerEnable << 1;
-
-  uint32 clock_speed = rcc_dev_timer_clk_speed(FrqDataPPM.TimerDev->clk_id);
-  timer->PSC	= (clock_speed/1000000)-1;
-  timer->ARR	= 0xffff;
-  timer->CR1	= 0;
-  timer->DIER &= ~(1);
-
-  timer->CCER &= ~TimerEnable; // Disable timer
-  timer->CCER &= ~(FrqDataPPM.PolarityMask);
-
-  volatile uint32 *mr;
-  if(aTimerChannel < 2) {
-    mr = &(timer->CCMR1);
-  } 
-  else {
-    mr = &(timer->CCMR2);
-  }
-  *mr &= ~(0xFF << (8*(aTimerChannel&1)));	// prescaler 1
-  *mr |= 0x61 << (8*(aTimerChannel&1));		// 0x61 -> 6=filter, 1=inputs 1,2,3,4
-
-  timer->CCER |= TimerEnable; // Enable
-  timer->CR1 = 1;
-
+    aTimerChannel--;  // transform timer channel numbering from 1-4 to 0-3
+    
+    FrqDataPPM.TimerDev     = aTimer;
+    timer_gen_reg_map *timer = aTimer->regs.gen;
+    FrqDataPPM.TimerRegs    = timer;
+    
+    FrqDataPPM.Timer_ccr    = &timer->CCR1 + aTimerChannel;
+    FrqDataPPM.TimerChannel = aTimerChannel;
+    
+    int TimerEnable = (1 << (4*aTimerChannel));
+    FrqDataPPM.PolarityMask = TimerEnable << 1;
+    
+    uint32 clock_speed = rcc_dev_timer_clk_speed(FrqDataPPM.TimerDev->clk_id);
+    timer->PSC	= (clock_speed/1000000)-1;
+    timer->ARR	= 0xffff;
+    timer->CR1	= 0;
+    timer->DIER &= ~(1);
+    
+    timer->CCER &= ~TimerEnable; // Disable timer
+    timer->CCER &= ~(FrqDataPPM.PolarityMask);
+    
+    volatile uint32 *mr;
+    if(aTimerChannel < 2) {
+        mr = &(timer->CCMR1);
+    }
+    else {
+        mr = &(timer->CCMR2);
+    }
+    *mr &= ~(0xFF << (8*(aTimerChannel&1)));	// prescaler 1
+    *mr |= 0x61 << (8*(aTimerChannel&1));		// 0x61 -> 6=filter, 1=inputs 1,2,3,4
+    
+    timer->CCER |= TimerEnable; // Enable
+    timer->CR1 = 1;
+    
 }
 
 void FrqChange()
 {
-  uint16_t c = *(FrqDataPPM.Timer_ccr);
-  uint16_t diffTime = c - FrqDataPPM.RiseTime;
-  if ((diffTime > 900) && (diffTime < 2100)) {
-    if (currentChannel < MAX_NB_CHANNEL) {
-      rawChannelValue[currentChannel] = diffTime;
-      currentChannel++;
+    uint16_t c = *(FrqDataPPM.Timer_ccr);
+    uint16_t diffTime = c - FrqDataPPM.RiseTime;
+    if ((diffTime > 900) && (diffTime < 2100)) {
+        if (currentChannel < MAX_NB_CHANNEL) {
+            rawChannelValue[currentChannel] = diffTime;
+            currentChannel++;
+        }
     }
-  } 
-  else if (diffTime > 2500) {
-    currentChannel = 0;
-  } 
-  else {
-    // glitch; stop and wait next round
-    currentChannel = MAX_NB_CHANNEL;
-  }
-  FrqDataPPM.RiseTime = c;
+    else if (diffTime > 2500) {
+        currentChannel = 0;
+    }
+    else {
+        // glitch; stop and wait next round
+        currentChannel = MAX_NB_CHANNEL;
+    }
+    FrqDataPPM.RiseTime = c;
 }
 
 void InitFrqMeasurementPPM()
 {
-  int pin = receiverPinPPM;
-  timer_dev *timer_num = PIN_MAP[pin].timer_device;
-
-  currentChannel=8;
-  if(timer_num != NULL) {
-    gpio_set_mode(PIN_MAP[pin].gpio_device, PIN_MAP[pin].gpio_bit, GPIO_AF_INPUT_PD);
-    FrqInit(1500, timer_num, PIN_MAP[pin].timer_channel);
-    timer_attach_interrupt(timer_num, PIN_MAP[pin].timer_channel, FrqChange);
-  }
-
+    int pin = receiverPinPPM;
+    timer_dev *timer_num = PIN_MAP[pin].timer_device;
+    
+    currentChannel=8;
+    if(timer_num != NULL) {
+        gpio_set_mode(PIN_MAP[pin].gpio_device, PIN_MAP[pin].gpio_bit, GPIO_AF_INPUT_PD);
+        FrqInit(1500, timer_num, PIN_MAP[pin].timer_channel);
+        timer_attach_interrupt(timer_num, PIN_MAP[pin].timer_channel, FrqChange);
+    }
+    
 }
 
 
@@ -318,15 +318,103 @@ void InitFrqMeasurementPPM()
 // interface part starts here
 
 void initializeReceiverPPM() {
-  
-  InitFrqMeasurementPPM();
+    
+    InitFrqMeasurementPPM();
 }
 
 
 int getRawChannelValuePPM(const byte channel) {
-  return rawChannelValue[ReceiverChannelMapPPM[channel]];
+    return rawChannelValue[ReceiverChannelMapPPM[channel]];
 }
 
+
+
+
+//
+// SBUS
+//
+static byte ReceiverChannelMapSBUS[MAX_NB_CHANNEL] = {XAXIS,YAXIS,THROTTLE,ZAXIS,MODE,AUX1,AUX2,AUX3,AUX4,AUX5,10,11,12,13,14,15,16,17};
+static unsigned int sbusIndex = 0;
+// sbus rssi reader variables
+static unsigned short sbusFailSafeCount = 0;
+static unsigned long sbusFrameCount = 0;
+static unsigned short sbusRate = 0;
+boolean UseSBUSRSSIReader;
+
+///////////////////////////////////////////////////////////////////////////////
+// implementation part starts here.
+
+void readSBUS()
+{
+    static byte sbus[25] = {0};
+    while(SERIAL_SBUS.available()) {
+        
+        int val = SERIAL_SBUS.read();
+        if(sbusIndex == 0 && val != SBUS_SYNCBYTE) {
+            continue;
+        }
+        
+        sbus[sbusIndex] = val;
+        sbusIndex++;
+        if (sbusIndex == 25) {
+            
+            sbusIndex = 0;
+            // check stop bit before updating buffers
+            if (sbus[24] == 0x0) {
+                
+                rawChannelValue[XAXIS]      = ((sbus[1]     | sbus[2]<<8)  & 0x07FF);					// pitch
+                rawChannelValue[YAXIS]      = ((sbus[2]>>3  | sbus[3]<<5)  & 0x07FF);					// roll
+                rawChannelValue[THROTTLE]   = ((sbus[3]>>6  | sbus[4]<<2   | sbus[5]<<10) & 0x07FF);	// throttle
+                rawChannelValue[ZAXIS]      = ((sbus[5]>>1  | sbus[6]<<7)  & 0x07FF);					// yaw
+                rawChannelValue[MODE]       = ((sbus[6]>>4  | sbus[7]<<4)  & 0x07FF);
+                rawChannelValue[AUX1]       = ((sbus[7]>>7  | sbus[8]<<1   | sbus[9]<<9) & 0x07FF);
+                rawChannelValue[AUX2]       = ((sbus[9]>>2  | sbus[10]<<6) & 0x07FF);
+                rawChannelValue[AUX3]       = ((sbus[10]>>5 | sbus[11]<<3) & 0x07FF);
+                rawChannelValue[AUX4]       = ((sbus[12]    | sbus[13]<<8) & 0x07FF);
+                rawChannelValue[AUX5]       = ((sbus[13]>>3 | sbus[14]<<5) & 0x07FF);
+                //rawChannelValue[AUX6]		= ((sbus[14]>>6 | sbus[15]<<2|sbus[16]<<10) & 0x07FF);
+                //rawChannelValue[AUX7]		= ((sbus[16]>>1 | sbus[17]<<7) & 0x07FF);
+                
+                
+                if (UseSBUSRSSIReader) {
+                    if (sbusRate == 0) {
+                        sbusFrameCount++;
+                    }
+                    if (((sbus[23] >> 3) & 0x0001)) {
+                        if ((sbusRate > 0) && (sbusFailSafeCount < sbusRate)) {
+                            sbusFailSafeCount++;
+                        }
+                    } else if (sbusFailSafeCount > 0) {
+                        sbusFailSafeCount--;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// interface part starts here
+
+void initializeReceiverSBUS() {
+    pinMode(BOARD_SPI2_NSS_PIN, OUTPUT);
+    digitalWrite(BOARD_SPI2_NSS_PIN,HIGH); // GPIO PB12 /Libmaple/libmaple/wirish/boards/aeroquad32.h line 69
+    SERIAL_SBUS.begin(100000);
+}
+
+// use this to switch from one receiver type to another?
+// re-enables serial port for other uses
+void terminateReceiverSBUS() {
+    SERIAL_SBUS.end();
+}
+
+int getRawChannelValueSBUS(const byte channel) {
+    if (channel == XAXIS) {
+        readSBUS();
+	}
+	return rawChannelValue[ReceiverChannelMapSBUS[channel]];
+}
 
 #endif
 
